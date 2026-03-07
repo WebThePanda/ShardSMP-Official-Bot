be5cb424c4f4fda81afb5ee8bedff47b1794259a
Sander <sander.schaass@gmail.com>
2026-03-07 17:25:48 +0100

    s

--- bot.py
+++ bot.py
@@ -24,6 +24,24 @@ bot = commands.Bot(command_prefix="b! ", intents=intents)
 
 # Commands
 
+@bot.command(name="sotw")
+@commands.has_role(1443544544409948272)
+async def sotw():
+    channel_id = 1479877488510500956
+    channel = bot.get_channel(channel_id)
+    panda = 502141502038999041
+    cats = 972943470023041044
+    bamboot = 1479571065549357362
+
+    embed = discord.Embed(
+        title="Staff of The Week",
+        description="Staff of The Week!\n \nVote for this weeks best staff!\n \nThis weeks choices are:\nWebThePanda (Owner)\nCats (Cool Guy)\nBamboot (Bot)",
+        colour=discord.Color.blurple()
+    )
+
+    if channel:
+        await channel.send(embed=embed)
+
 @bot.command(name="purge")
 @commands.has_permissions(manage_messages=True)
 async def purge(ctx, amount: int):
@@ -55,7 +73,7 @@ async def on_member_join(member):
         name=f"New Member: {member.display_name}",
         icon_url=member.display_avatar.url
     )
-    embed.set_footer(text="Made by WebThePanda")
+    
     if channel:
         await channel.send(embed=embed)
 
